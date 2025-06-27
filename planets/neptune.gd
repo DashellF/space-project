014a75ad216c37d21344370892724ffe35001f11
murk1sh <ernesthezhang@gmail.com>
2025-06-27 06:02:52 -0700

    Neptune Axial tilt

--- planets/neptune.gd
+++ planets/neptune.gd
@@ -4,21 +4,23 @@ extends MeshInstance3D
 var custom_axis: Vector3
 
 func _ready():
-	var y_rot = deg_to_rad(270)
-	var z_rot = deg_to_rad(28.3)
-
-	# start with UP axis
-	var axis = Vector3.UP
-	
-	var y_basis = Basis(Vector3.UP, y_rot)
-	axis = y_basis * axis 
-
-	var z_basis = Basis(Vector3.RIGHT, z_rot)
-	axis = z_basis * axis  
-
-	custom_axis = axis.normalized()
+	rotation_degrees.x=28.32
+	#var y_rot = deg_to_rad(270)
+	#var z_rot = deg_to_rad(28.3)
+#
+	## start with UP axis
+	#var axis = Vector3.UP
+	#
+	#var y_basis = Basis(Vector3.UP, y_rot)
+	#axis = y_basis * axis 
+#
+	#var z_basis = Basis(Vector3.RIGHT, z_rot)
+	#axis = z_basis * axis  
+#
+	#custom_axis = axis.normalized()
 
 func _process(delta):
-	var angle = rotation_speed * delta
-	var q = Quaternion(custom_axis, angle)
-	global_transform.basis = Basis(q) * global_transform.basis
+	rotate_object_local(Vector3.UP, rotation_speed * delta)
+	#var angle = rotation_speed * delta
+	#var q = Quaternion(custom_axis, angle)
+	#global_transform.basis = Basis(q) * global_transform.basis
