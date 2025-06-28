d782620a272ccf25ac50991aa367e577e76185d8
DashellF <djf1517@gmail.com>
2025-06-27 19:14:11 -0700

    added rotation to moons

---
+++ planets/phobos.gd
@@ -0,0 +1,29 @@
+extends MeshInstance3D
+
+@export var rotation_speed :=  0.821  # Radians per hour
+var custom_axis: Vector3
+
+func _ready():
+	if not global.is_connected("timespeed_updated", Callable(self, "_on_timespeed_updated")):
+		global.connect("timespeed_updated", Callable(self, "_on_timespeed_updated"))
+	var y_rot = deg_to_rad(105)
+	var z_rot = deg_to_rad(25.2)
+
+	# start with UP axis
+	var axis = Vector3.UP
+	
+	var y_basis = Basis(Vector3.UP, y_rot)
+	axis = y_basis * axis 
+
+	var z_basis = Basis(Vector3.RIGHT, z_rot)
+	axis = z_basis * axis  
+
+	custom_axis = axis.normalized()
+	
+func _on_timespeed_updated(new_value: float):
+	rotation_speed =  0.821 * global.timeSpeed
+	
+func _process(delta):
+	var angle = rotation_speed * delta
+	var q = Quaternion(custom_axis, angle)
+	global_transform.basis = Basis(q) * global_transform.basis
